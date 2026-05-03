fd1d68e8e51f7819cc612cf35da1df5e400b6eea
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-05-03 14:51:28 +0930

    Hero section completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,10 @@ export default function Home() {
       <div className="container">
         <div className="info">
           <h1>Hi I'm Ashish</h1>
-          <h2>Aspiring SOftware Developeer</h2>
+          <h2>Aspiring Software Engineer</h2>
           <p>Hi I'm Ashish, an aspiring Software engineer with a focus on full-stack development and AI, I like tackling hard problems and building things that actually work.</p>
-    </div>
-        
+          <a className="exp-button" href="#experience">Experience</a>
+    </div> 
       </div>
     </section>
     
