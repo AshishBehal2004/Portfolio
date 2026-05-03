b027cb8fbe486745f0049320abd6df70951e6710
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-05-03 15:08:31 +0930

    About section work in progress

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,21 @@
 export default function About() {
   return (
     <div id="about">
-        <p className="text-4xl"></p>
+        <p className="text-4xl">
+Hi, I'm Ashish Behal, currently in my final year pursuing a Bachelor of IT (Software Development) at the Adelaide University.
+ I am a motivated individual with a passion for problem-solving, software development, and building things that are both functional 
+ and well-crafted. I consider myself a lifelong learner, always seeking to grow and connect with like-minded people in the tech community.
+ <br></br><br></br>
+With hands-on experience in the software industry, I have a proven ability to adapt quickly to new technologies, 
+with a solid background in modern software development practices, full-stack web development, Unity, and cloud platforms. 
+I have a particular interest in backend systems and writing clean, efficient code, and I consistently aim to deliver robust 
+and scalable solutions.
+
+<br></br><br></br> Java is my primary language for problem-solving, backed by 70+ LeetCode problems and a strong foundation
+ in data structures and algorithms.
+ Outside of work, I enjoy gaming, reading books about cosmoss/universe, and learning new trends in the technology.
+
+ </p>
     </div>
   );
 }
