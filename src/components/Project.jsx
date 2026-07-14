60e9063392fa9d6fc16b1fdb96d6c6cf88f9f3e0
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-07-14 15:48:37 +0930

    Remove Project Effect, Shifted to Static Card Layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,51 +1,29 @@
 import {projects} from './projectData'
 import './project.css'
-import { useState, useEffect } from 'react';
-
 export default function Project() {
 
-  const flipProjects = new Array(projects.length)
-  for (let i = 0; i < projects.length; i++ ){
-    flipProjects[i] = false;
-  }
-  const [flipState, setFlipState] = useState(flipProjects)
-  
   return (
     <div id="project">
       <h2 className="section-heading">Projects</h2>
 
         <div className='project-wrapper'>
           {projects.map((item, index) => (
-            <div className="card-container" key={item.projectName} onMouseMove={(e) => {const rect = e.currentTarget.getBoundingClientRect()
-              const centerX= rect.left + rect.width / 2
-              const centerY = rect.top + rect.height / 2
-
-            }
-            } onClick={() => {
-              const newFlipState = [...flipState]
-              newFlipState[index] = !newFlipState[index]
-              setFlipState(newFlipState)
-              }}>
-              <div className={flipState[index] ? "inner-card flipped" : "inner-card" }>
-                
-              <div className="front-face">
+            <div className="project-card" key={item.projectName}>   
+              <div className='project-image' > </div>
+              <div className="project-info">
                 <h3>{item.projectName}</h3>
-                <p>{item.tagline}</p>
+                <p className='project-tagline'>{item.tagline}</p>
                 <a href={item.projectLink} target='_blank' rel='noreferrer'>View Project</a>
-
-              </div>
-              <div className="back-face">
                 {item.date}
-                {item.frameworks.map((framework) => (
+                <div className='tech-tags'>{item.frameworks.map((framework) => (
                   <span key={framework}>{framework}</span>
-                ))}
-                {item.content.map((point)=> (
-                  <span key={point}>{point}</span>
-                ))}
-
+                ))}</div>
+                <ul className='project-content'>{item.content.map((point)=> (
+                  <li key={point}>{point}</li>
+                ))}</ul>
               </div>
+              
             </div>
-          </div>
           ))}
         </div>
     </div>
