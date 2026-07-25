ff0ae45294ef17088bf7a6647e6198875869ac18
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-07-25 15:38:32 +0930

    Other Projects Details Added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     <div id="about">
       <h2 className="section-heading">About</h2>
       <div className="content">
-        <p className='heading'>I'm Ashish, currently finishing a Software Engineering (Honours) degree at Adelaide University. I got into this field because I like taking things apart to understand how they actually work, and that's stuck with me ever since, especially on the backend side.</p>
+        <p className='heading'>I'm Ashish, currently finishing a Bachelor of IT majoring in Software Development degree at Adelaide University. I got into this field because I like taking things apart to understand how they actually work, and that's stuck with me ever since, especially on the backend side.</p>
  <br></br>
  <p>I've spent the last year or so building real, deployed projects rather than just coursework like machine learning pipelines, interactive front-end interfaces, and everything in between. Most of my Java and DSA foundation actually came from Adelaide's competitive programming community rather than solo studying. I'd rather work through hard problems with people than alone.</p>
  <br></br>
